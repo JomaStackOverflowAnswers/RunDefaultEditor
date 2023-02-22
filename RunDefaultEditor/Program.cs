a902d005094e224e15eef998700ee84813d388b0
José Espinoza Bone <megamanx@outlook.com>
2023-02-21 23:20:52 -0500

    Add EditorInfo class and Editor variables

--- RunDefaultEditor/Program.cs
+++ RunDefaultEditor/Program.cs
@@ -1,50 +1,120 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
-//Launcher.LaunchEditor("File.txt", "code", false, true); //With Verbose
-//Launcher.LaunchEditor("File.txt", "notepad", false, true);
-//Launcher.LaunchEditor("File.txt", "ignored-command-name", TryVsCode: true, Verbose: true); //Ignore editor, prefers VSCode.
-//Launcher.LaunchEditor("File.txt", "bad-command-name", TryVsCode: false, Verbose: true); //Bad command name. Try to launch the default.
-//Launcher.LaunchEditor("File.txt", "C:/Program Files/Notepad++/notepad++.exe", Verbose: true);
-//Launcher.LaunchEditor("File.txt", "C:/Program Files/Sublime Text/sublime_text.exe", Verbose: true);
-//Launcher.LaunchEditor("File.txt", "vim", Verbose: true);
-//Launcher.LaunchEditor("File.txt", "vi", Verbose: true); 
-//Launcher.LaunchEditor("File.txt", "nano", Verbose: true); 
-//Launcher.LaunchEditor("File.txt", "", Verbose: true); 
-
 Console.WriteLine("███ Opening file with text Editor");
-string editor = "";
+string editor = string.Empty;
+bool wait = false;
+string parameters = string.Empty;
 while (!editor.Equals("exit!"))
 {
-    Console.WriteLine(@"Type the editor command/filename OR exit [exit!]:");
+    Console.WriteLine(@"Type the editor command/filename OR exit [exit]:");
     editor = Console.ReadLine() ?? string.Empty;
-    if (!editor.Equals("exit!"))
+    if (editor.Equals("exit"))
     {
-        Launcher.LaunchEditor("File.txt", editor, verbose: true);
+        break;
     }
+    Console.WriteLine(@"Type [True] OR [False] to set if the editor is command line app:");
+    wait = bool.TryParse(Console.ReadLine(), out bool result);
+    Console.WriteLine(@"Type additional parameters:");
+    parameters = Console.ReadLine() ?? string.Empty;
+
+    await EditorLauncher.LaunchAsync(new EditorInfo { Command = editor, IsCommandLineApp = wait, Parameters = parameters }, "File.txt", verbose: true);
+}
+//Try to Launch multiple editors.
+//await EditorLauncher.LaunchAsync(EditorLauncher.NotepadEditor, "File.txt", true);
+//await EditorLauncher.LaunchAsync(EditorLauncher.VisualStudioEditorWindows, "File.txt", true);
+//await EditorLauncher.LaunchAsync(EditorLauncher.NotepadPlusPlusEditor, "File.txt", true);
+//await EditorLauncher.LaunchAsync(EditorLauncher.SublimeTextEditorWindows, "File.txt", true);
+//await EditorLauncher.LaunchAsync(EditorLauncher.NotepadPlusPlusEditorFullPath, "File.txt", true);
+//await EditorLauncher.LaunchAsync(EditorLauncher.SublimeTextEditorWindowsFullPath, "File.txt", true);
+//await EditorLauncher.LaunchAsync(EditorLauncher.CodeEditor, "File.txt", true);
+//await EditorLauncher.LaunchAsync(EditorLauncher.SublimeTextEditorUnix, "File.txt", true);
+//await EditorLauncher.LaunchAsync(EditorLauncher.NanoEditor, "File.txt", true);
+//await EditorLauncher.LaunchAsync(EditorLauncher.VimEditor, "File.txt", true);
+//await EditorLauncher.LaunchAsync(EditorLauncher.ViEditor, "File.txt", true);
+//await EditorLauncher.LaunchAsync(EditorLauncher.OpenDefaultEditorMacOS, "File.txt", true);
+//await EditorLauncher.LaunchAsync(EditorLauncher.OpenTextEditEditorMacOS, "File.txt", true);
+//await EditorLauncher.LaunchAsync(new EditorInfo { Command ="gedit", WaitForExit = false, Parameters = "-s"}, "File.txt", true);
+
+public class EditorInfo
+{
+    public string Command { get; init; } = null!;
+    public bool IsCommandLineApp { get; init; } = false;
+    public string Parameters { get; set; } = string.Empty;
 }
 
-
-public class Launcher
+public class EditorLauncher
 {
-    private static void OpenProcess(string command, string parameters, string textFilename, bool verbose)
+    public static readonly EditorInfo NotepadEditor = new() { Command = "notepad" };
+    public static readonly EditorInfo VisualStudioEditorWindows = new() { Command = "devenv.exe" };
+    public static readonly EditorInfo NotepadPlusPlusEditor = new() { Command = "notepad++.exe" };
+    public static readonly EditorInfo SublimeTextEditorWindows = new() { Command = "subl.exe" };
+    public static readonly EditorInfo NotepadPlusPlusEditorFullPath = new() { Command = "C:/Program Files/Notepad++/notepad++.exe" };
+    public static readonly EditorInfo SublimeTextEditorWindowsFullPath = new() { Command = "C:/Program Files/Sublime Text/sublime_text.exe" };
+    public static readonly EditorInfo CodeEditor = new() { Command = "code" };
+    public static readonly EditorInfo SublimeTextEditorUnix = new() { Command = "subl" };
+    public static readonly EditorInfo NanoEditor = new() { Command = "nano", IsCommandLineApp = true };
+    public static readonly EditorInfo VimEditor = new() { Command = "vim", IsCommandLineApp = true };
+    public static readonly EditorInfo ViEditor = new() { Command = "vi", IsCommandLineApp = true };
+    public static readonly EditorInfo OpenDefaultEditorMacOS = new() { Command = "open", IsCommandLineApp = true, Parameters = @"-t" };
+    public static readonly EditorInfo OpenTextEditEditorMacOS = new() { Command = "open", IsCommandLineApp = true, Parameters = @"-e" };
+
+    private static readonly ReadOnlyDictionary<string, EditorInfo> WindowsEditors = new ReadOnlyDictionary<string, EditorInfo>(
+        new Dictionary<string, EditorInfo>
+        {
+            [NotepadEditor.Command] = NotepadEditor,
+            [CodeEditor.Command] = CodeEditor,
+            [NotepadPlusPlusEditor.Command] = NotepadPlusPlusEditor,
+            [SublimeTextEditorWindows.Command] = SublimeTextEditorWindows,
+            [NotepadPlusPlusEditorFullPath.Command] = NotepadPlusPlusEditor,
+            [SublimeTextEditorWindowsFullPath.Command] = SublimeTextEditorWindows,
+            [VisualStudioEditorWindows.Command] = VisualStudioEditorWindows,
+            [VimEditor.Command] = VimEditor
+        });
+
+    private static readonly ReadOnlyDictionary<string, EditorInfo> LinuxEditors = new ReadOnlyDictionary<string, EditorInfo>(
+        new Dictionary<string, EditorInfo>
+        {
+            [nameof(NanoEditor)] = NanoEditor,
+            [nameof(VimEditor)] = VimEditor,
+            [nameof(ViEditor)] = ViEditor,
+            [nameof(CodeEditor)] = CodeEditor,
+            [nameof(SublimeTextEditorUnix)] = SublimeTextEditorUnix
+        });
+
+    private static readonly ReadOnlyDictionary<string, EditorInfo> MacOSEditors = new ReadOnlyDictionary<string, EditorInfo>(
+        new Dictionary<string, EditorInfo>
+        {
+            [nameof(OpenDefaultEditorMacOS)] = OpenDefaultEditorMacOS,
+            [nameof(OpenTextEditEditorMacOS)] = OpenTextEditEditorMacOS,
+            [nameof(NanoEditor)] = NanoEditor,
+            [nameof(VimEditor)] = VimEditor,
+            [nameof(ViEditor)] = ViEditor,
+            [nameof(CodeEditor)] = CodeEditor,
+            [nameof(SublimeTextEditorUnix)] = SublimeTextEditorUnix
+        });
+
+    private static async Task OpenProcessAsync(EditorInfo editor, string textFilename, bool verbose)
     {
-        if (!string.IsNullOrWhiteSpace(command))
+        if (!string.IsNullOrWhiteSpace(editor.Command))
         {
             if (verbose)
             {
-                Console.WriteLine($@"Opening ""{textFilename}"" with ""{command}"" editor.");
+                Console.WriteLine($@"Opening ""{textFilename}"" with ""{editor.Command}"" {(editor.IsCommandLineApp? "command line " : string.Empty)}editor.");
             }
             var process = new Process();
             process.StartInfo.UseShellExecute = true;
-            process.StartInfo.FileName = command;
-            process.StartInfo.Arguments = parameters;
+            process.StartInfo.FileName = editor.Command;
+            process.StartInfo.Arguments = @$"{editor.Parameters} ""{textFilename}""";
             process.Start();
+            if (editor.IsCommandLineApp)
+            {
+                await process.WaitForExitAsync();
+            }
+            return;
         }
-        else
-        {
-            Console.WriteLine(@$"No editor was found for file ""{textFilename}"".");
-        }
+        Console.WriteLine(@$"Invalid command.");
     }
 
     /// <summary>
@@ -54,63 +124,72 @@ public class Launcher
     /// <param name="editor">The editor command.</param>
     /// <param name="tryVsCode">If Visual Studio Code is available. The file is opened with VSCode. Editor value is ignored.</param>
     /// <param name="verbose">Prints parameters.</param>
-    public static void LaunchEditor(string textFilename, string editor = "", bool tryVsCode = false, bool verbose = false)
+    public static async Task LaunchAsync(EditorInfo editor, string textFilename, bool tryVsCode = false, bool verbose = false)
     {
-        var command = CommandExists(editor);
-        List<string> windowsCommands = new List<string> { "notepad", "code" };
-        List<string> linuxCommands = new List<string> { "nano", "vim", "vi" };
-
+        string command;
         if (tryVsCode)
         {
-            command = CommandExists("code");
+            command = CommandExists(CodeEditor.Command);
             if (!string.IsNullOrWhiteSpace(command))
             {
-                OpenProcess(command, @$"""{textFilename}""", textFilename, verbose);
+                await OpenProcessAsync(CodeEditor, textFilename, verbose);
                 return;
             }
         }
 
-        command = CommandExists(editor);
+        command = CommandExists(editor.Command);
         if (!string.IsNullOrWhiteSpace(command))
         {
-            OpenProcess(command, @$"""{textFilename}""", textFilename, verbose);
+            await OpenProcessAsync(editor, textFilename, verbose);
             return;
         }
-
+        string message = @$"No editor was found for file ""{textFilename}"".";
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
-            foreach (var cmd in windowsCommands)
+            foreach (var cmd in WindowsEditors)
             {
-                command = CommandExists(cmd);
+                command = CommandExists(cmd.Value.Command);
                 if (!string.IsNullOrWhiteSpace(command))
                 {
-                    OpenProcess(command, @$"""{textFilename}""", textFilename, verbose);
+                    await OpenProcessAsync(cmd.Value, textFilename, verbose);
                     return;
                 }
             }
+            Console.WriteLine(message);
         }
 
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
         {
-            foreach (var cmd in linuxCommands)
+            foreach (var cmd in LinuxEditors)
             {
-                command = CommandExists(cmd);
+                command = CommandExists(cmd.Value.Command);
                 if (!string.IsNullOrWhiteSpace(command))
                 {
-                    OpenProcess(command, @$"""{textFilename}""", textFilename, verbose);
+                    await OpenProcessAsync(cmd.Value, textFilename, verbose);
                     return;
                 }
             }
+            Console.WriteLine(message);
         }
 
         if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
         {
-            OpenProcess("open", @$"-t ""{textFilename}""", textFilename, verbose);
+            foreach (var cmd in MacOSEditors)
+            {
+                command = CommandExists(cmd.Value.Command);
+                if (!string.IsNullOrWhiteSpace(command))
+                {
+                    await OpenProcessAsync(cmd.Value, textFilename, verbose);
+                    return;
+                }
+            }
+            Console.WriteLine(message);
         }
+        throw new NotSupportedException(RuntimeInformation.OSDescription);
     }
 
 
-    static string CommandExists(string filename)
+    private static string CommandExists(string filename)
     {
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
